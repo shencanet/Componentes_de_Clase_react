603c3c5baaef0be1d0895e1465165cae30b7bc17
shencanet <santi.dionis@gmail.com>
2022-08-06 12:52:19 +0200

    ADD :clase testimonio

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -2,22 +2,29 @@
 import React from 'react';
 import './Testimonio.css';
 
-function Testimonio(props) {
-	return (
-		<div className="contenedor-testimonio">
-			<img className='imagen-testimonio'
-				src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-				alt='foto perfil' />
+class Testimonio extends React.Component {
+	render() {
+		return (
+			<div className="contenedor-testimonio">
+				<img className='imagen-testimonio'
+					src={require(`../imagenes/testimonio-${this.props.imagen}.png`)}
+					alt='foto perfil' />
 
-			<div className='contenedor-texto-testimonio'>
-				<p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
-				<p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
-				<p className='texto-testominio'>{props.testimonio}</p>
+				<div className='contenedor-texto-testimonio'>
+					<p className='nombre-testimonio'><strong>{this.props.nombre}</strong> en {this.props.pais}</p>
+					<p className='cargo-testimonio'>{this.props.cargo} en <strong>{this.props.empresa}</strong></p>
+					<p className='texto-testominio'>{this.props.testimonio}</p>
+
+				</div>
 
 			</div>
+		);
 
-		</div>
-	);
+	}
+
 }
+
+
+
 
 export default Testimonio;
